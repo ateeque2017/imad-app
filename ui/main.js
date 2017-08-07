9659b842359b1d1168d093295561ce8ebdfeb71a
ateeque2017 <30381323+ateeque2017@users.noreply.github.com>
2017-08-07 16:38:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById("main-txt");
 element.innerHTML="NEW Value";
+var img = document.getElementById("madi");
+img.onclick = function () {
+    img.style.marginleft = '100px';
+    };
