aa39a0fdc03bfbfd165e3334aefd7fe8b11edae8
ateeque2017 <30381323+ateeque2017@users.noreply.github.com>
2017-08-08 11:38:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,7 @@
-console.log('Loaded!');
-var img = document.getElementById("madi");
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft+5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight,50);
+var button = document.getElementById("counter");
+var counter = 0;
+button.onclick = function () {
+    var span = document.getElementById("span");
+    counter = counter + 1;
+    span.innerHTML = counter.toString();
     };
